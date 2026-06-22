f696775f330c7d87ac1b1f96f51e3d0e67f6fb40
Prince Vadher <vadherprince63@gmail.com>
2026-06-23 00:42:30 +0530

    Optimize email sender

--- backend/utils/EmailSender.js
+++ backend/utils/EmailSender.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_HOST,
         pass: process.env.EMAIL_PASSWORD,
@@ -19,13 +21,12 @@ const EmailSender = async (to, sub, content) => {
             html: content
         });
 
-        console.log("Email sent successfully");
+        console.log("Email sent");
 
     } catch (error) {
         console.error("Error sending email:", error);
         throw new Error("Failed to send email");
     }
 };
-
 
 module.exports = EmailSender;
