5449a5115e92f0c29c13c57331803dfe69d95e28
Prince Vadher <vadherprince63@gmail.com>
2026-06-23 23:27:52 +0530

    Optimize email sender

--- backend/utils/EmailSender.js
+++ backend/utils/EmailSender.js
@@ -10,6 +10,13 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP Error:", error);
+  } else {
+    console.log("SMTP Server is Ready");
+  }
+});
 
 const EmailSender = async (to, sub, content) => {
     try {
