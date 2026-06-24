249db7101ab1fd120229ec6bbcd788049ce68f3e
Prince Vadher <vadherprince63@gmail.com>
2026-06-24 21:19:48 +0530

    Optimize email sender

--- backend/utils/EmailSender.js
+++ backend/utils/EmailSender.js
@@ -1,13 +1,18 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    host: "smtp-relay.brevo.com",
+    port: 465,
+    secure: true,
     auth: {
-        user: process.env.EMAIL_HOST,
-        pass: process.env.EMAIL_PASSWORD,
-    }
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS,
+    },
+
+    connectionTimeout: 10000,
+    tls: {
+    rejectUnauthorized: false 
+  }
 });
 
 
